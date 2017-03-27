7c03b42be9f1fe260020fc65e82a89a4381ee193
guoyongwei <Gyw56Gm#>
2017-03-27 17:58:13 +0800

    helloword

---
+++ objSort.js
@@ -0,0 +1,18 @@
+computed: {
+            orderList: function () {
+                var compare = function (idx) {
+                    return function (object1, object2) {
+                        var a = object1[idx];
+                        var b = object2[idx];
+                        if (b < a) {
+                            return -1;
+                        } else if (b > a) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    };
+                };
+                return this.list.sort(compare('crtDate'));
+            }
+        },
